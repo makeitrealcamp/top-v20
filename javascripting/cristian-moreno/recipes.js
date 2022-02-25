e7165125d829161bd6483417af7939e446aec16d
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-02-25 14:53:01 -0500

    feat: add recipe example

---
+++ javascripting/cristian-moreno/recipes.js
@@ -0,0 +1,11 @@
+const recipe = {
+  name: "",
+  ingredients: [
+    { name: "Pan", quantity: 2 },
+    { name: "Jamón", quantity: 1 },
+    { name: "Queso", quantity: 1 },
+  ]
+}
+
+// El primer ingrendiente (name) de la receta
+console.log(recipe.ingredients[0].name)
