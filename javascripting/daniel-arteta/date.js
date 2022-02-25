be7e0c4af77b3828b39ea96531e8bf51adcc78ca
Daniel <danielatts97@hotmail.com>
2022-02-25 15:45:35 -0500

    Add date exercise

---
+++ javascripting/daniel-arteta/date.js
@@ -0,0 +1,12 @@
+const { format, compareAsc } = require('date-fns')
+
+format(new Date(2014, 1, 11), 'MM/dd/yyyy')
+//=> '02/11/2014'
+
+const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+]
+console.log(dates.sort(compareAsc))
+
