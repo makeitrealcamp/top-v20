ff99ae9d421288b52127af2df7b9f19e85d15474
Emerson Asto <emerar.mct@gmail.com>
2022-02-25 14:48:05 -0500

    print first ingredient

---
+++ javascripting/emerson-asto/recipes.js
@@ -0,0 +1,10 @@
+const recipe = {
+  name: "",
+  ingredients: [
+    { name: "Pan", quantity: 2 },
+    { name: "Jamón", quantity: 1 },
+    { name: "Queso", quantity: 1 },
+  ]
+}
+
+console.log(recipe.ingredients[0].name);
