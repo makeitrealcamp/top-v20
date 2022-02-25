09a71dacb1a101f3a70c8c77036180168eb5186a
Raúl Lara <ralpwd@gmail.com>
2022-02-25 14:48:58 -0500

    Realizado primer ejercicio

---
+++ javascripting/raul-lara/recipes.js
@@ -0,0 +1,10 @@
+const recipe = {
+    name: "Raúl Lara",
+    ingredients: [
+      { name: "Pan", quantity: 2 },
+      { name: "Jamón", quantity: 1 },
+      { name: "Queso", quantity: 1 },
+    ]
+  }
+
+  console.log(recipe.ingredients[0].name)
