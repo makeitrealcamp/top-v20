b3d3b9164039cb109c1b365b3a5f132f50bc6da0
jose_miguel <josecastrillong@gmail.com>
2022-02-27 18:11:25 -0500

    feat: add recipe example

---
+++ javascripting/jose-castrillon/recipes.js
@@ -0,0 +1,11 @@
+const recipe = {
+  name: "",
+  ingredients: [
+    { name: "Pan", quantity: 2 },
+    { name: "Jamón", quantity: 1 },
+    { name: "Queso", quantity: 1 },
+  ]
+}
+
+//prints the first value of the recipe
+console.log(recipe.ingredients[0].name)
