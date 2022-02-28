8ef5b7a54d24b21fd8c1607e9f7527c1d7a8c7f9
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-28 11:00:24 -0500

    feature 8 : change number a string

---
+++ javascripting/alex-suarez/number-to-string.js
@@ -0,0 +1,28 @@
+/*
+ ## NÚMERO A STRING (Ejercicio 8 de 20)
+
+  A veces necesitarás convertir un número a una string.
+
+  En esos casos, usarás el método .toString(). A continuación un ejemplo:
+
+     const n = 256
+     n.toString()
+
+ ## El ejercicio
+
+  Crea un archivo llamado number-to-string.js.
+
+  En ese archivo define una variable llamada n que referencie el número 128;
+
+  LLama al método .toString() de esa variable n.
+
+  Utiliza console.log() para imprimir los resultados de .toString() a la
+  terminal.
+
+  Comprueba si tu programa es correcto ejecutando el siguiente comando:
+
+     javascripting verify number-to-string.js
+
+*/
+const n=128;
+console.log(n.toString());
