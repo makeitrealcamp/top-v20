05a089a1ac6bc2d6e3ac6792825b30513101d6f8
borismg04 <borismg04@gmail.com>
2022-02-28 11:05:14 -0500

    Feat:inf condiciones caracteres

--- javascripting/boris-monroy/if-statement.js
+++ javascripting/boris-monroy/if-statement.js
@@ -1,6 +1,6 @@
 let fruit="orange"
-if(fruit > 1){
+if(fruit.length >5){
     console.log("The fruit name has more than five characters.");
 }else{
-    console.log("The fruit name has five characters or less.");
+    console.log("The fruit name has five characters.");
 }
