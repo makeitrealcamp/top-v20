fc478c43b1b068b48f439108a1941e2a37a0cc86
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-28 09:13:29 -0500

    exercise 1 of guia javascripting

---
+++ javascripting/alex-suarez/introduction.js
@@ -0,0 +1,35 @@
+/*
+ ## INTRODUCCIÓN (Ejercicio 1 de 20)
+
+  Para mantener el orden, procederemos a crear una carpeta para este
+  workshop.
+
+  Ejecuta el siguiente comando, cambiando el nombre de la carpeta o
+  colocando el path que necesites:
+
+     mkdir javascripting
+
+  Cambia de directorio a la carpeta que acabas de crear:
+
+     cd javascripting
+
+  Crea un archivo llamado introduction.js utilizando:
+
+     touch introduction.js
+
+  , o si utilizas Windows
+
+     type NUL > introduction.js
+
+  (type es parte del comando!)
+
+  Agrega el siguiente texto al archivo:
+
+     console.log('hello')
+
+  Comprueba si tu programa es correcto ejecutando el siguiente comando:
+
+     javascripting verify introduction.js
+
+     */
+    console.log('hello')
