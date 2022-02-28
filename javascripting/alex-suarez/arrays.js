9034cb92fd44736446d34044d2a6bbe3cd3d1920
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-28 13:09:56 -0500

    feature 11: programa que imprime un arreglo de string

---
+++ javascripting/alex-suarez/arrays.js
@@ -0,0 +1,24 @@
+/*
+ ## ARRAYS (Ejercicio 11 de 20)
+
+  Un array es una lista ordenada de elementos. Por ejemplo:
+
+     const pets = ['cat', 'dog', 'rat']
+
+ ### El ejercicio:
+
+  Crea un archivo llamado arrays.js
+
+  En ese archivo define una variable llamada pizzaToppings que referencie a
+  un array el cual contenga los siguientes elementos (en el siguiente
+  orden): tomato sauce, cheese, pepperoni.
+
+  Utiliza console.log() para imprimir la variable pizzaToppings a la
+  terminal.
+
+  Comprueba si tu programa es correcto ejecutando el siguiente commando:
+
+     javascripting verify arrays.js
+*/
+let pizzaToppings=['tomato sauce','cheese','pepperoni']
+console.log(pizzaToppings)
