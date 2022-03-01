cda8abc69935e4499545ef70ab73fa4782c806e1
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-03-01 12:46:53 -0500

    feat: exercise 18 get date of function and print for console the date

--- javascripting/alex-suarez/function.js
+++ javascripting/alex-suarez/function.js
@@ -1,44 +1,4 @@
-/*
- ## FUNCIONES (Ejercicio 18 de 20)
-
-  Una función es un bloque de código que puede recibir un input y devolver
-  un output.
-
-  Vamos a utilizar la palabra reservada return para especificar lo que
-  devuelve una función.
-
-  Por ejemplo:
-
-     function example (x) {
-       return x * 2
-     }
-
-  Podemos llamar a la función de esta forma para obtener el número 10:
-
-     console.log(example(5))
-
-  El ejemplo anterior asume que la función example recibirá un número cómo
-  argumento –– input –– y retornará el número multiplicado por 2.
-
- ## El ejercicio:
-
-  Crea una archivo llamando functions.js
-
-  En ese archivo, define una función llamada eat que reciba un argumento
-  llamado food que será una string.
-
-  Dentro de la función, retorna el argumento food de la siguiente manera:
-
-     return food + ' tasted really good.'
-
-  Dentro de los paréntesis de console.log(), llama a la función eat() con la
-  string bananas cómo argumento.
-
-  Comprueba si tu programa es correcto ejecutando el siguiente comando:
-
-     javascripting verify functions.js
-*/
 function eat(food){
-  return food +' tasted really good.'
+  return food +' tasted really good.';
 }
-console.log(eat('bananas'))
+console.log(eat('bananas'));
