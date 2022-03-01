da8676886c1710c0721e041c32a683f91e992e35
Emerson Asto <emerar.mct@gmail.com>
2022-03-01 11:29:10 -0500

    make-it-modular learnyounode

---
+++ learnyounode/emerson-asto/make-it-modular.js
@@ -0,0 +1,11 @@
+const mymodule = require('./mymodule.js');
+
+mymodule(process.argv[2], process.argv[3], (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    data.forEach((file) => {
+      console.log(file);
+    });
+  }
+})
