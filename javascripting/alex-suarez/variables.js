7b81fea62edbf5badc6ad963a5841b95ab989a42
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-03-01 12:15:55 -0500

    feat: exercise 2 print a string

--- javascripting/alex-suarez/variables.js
+++ javascripting/alex-suarez/variables.js
@@ -1,22 +1,2 @@
-/*
- ## VARIABLES (Ejercicio 2 de 20)
-
-  Una variable es una referencia a un valor. Define una variable usando la
-  palabra reservada let.
-
-  Por ejemplo:
-
-     let example
-
-  La variable anterior es declarada, pero no definida.
-
-  A continuación damos un ejemplo de cómo definir una variable, haciendo que
-  referencie a un valor específico:
-
-     const example = 'some string'
-
-  Nota que empieza con la palabra reserva let y usa el signo de igualdad
-  entre en nombre de la variable y el valor que referencia.
-*/
 let example='some string'
 console.log(example)
