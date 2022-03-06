edf59ed916ba8e5ada813cb8a0994e1890650d33
carlos <carlosjuniorrvega@hotmail.com>
2022-03-06 15:54:21 -0500

    javascripting:if-statement.js

---
+++ javascripting/carlos-vega/if-statement.js
@@ -0,0 +1,7 @@
+const fruit = 'orange';
+const longitud = fruit.length;
+if(longitud>5){
+    console.log('The fruit name has more than five characters.');
+} else {
+    console.log( 'The fruit name has five characters or less.');
+}
