201b33711653483fbfef0bf79e96675f1234f350
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-04-28 12:29:38 -0500

    feat: add sockets

--- examples/web-sockets/socketio/index.js
+++ examples/web-sockets/socketio/index.js
@@ -1,39 +1,18 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const { faker } = require('@faker-js/faker');
 
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-const messages = [
-  {
-    id: 1,
-    text: 'Hello World',
-    author: '@khriztianmoreno'
-  }
-]
-
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.get('/hello', (req, res) => {
-  res.status(200).json({ msg: 'Hello World!' });
-})
-
-io.on('connection', (socket) => {
-  // Recibimos una nueva conexión
-  console.log('New connection', socket.id);
-
-  io.emit('messages', messages);
-
-  socket.on('message:add', (message) => {
-    console.log('New message', message);
-    messages.push(message);
-
-    io.emit('messages', messages);
-  })
+  res.status(200).send('Hello World');
 })
 
 server.listen(8080, () => {
-  console.log('Server is running on http://localhost:8080');
+  console.log('Server is running http://localhost:8080');
 })
